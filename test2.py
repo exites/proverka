b2e9ce2aab787b6120a9a763698f61243b76e924
Ex <or.strangelove@gmail.com>
2021-10-22 21:12:40 +0300

    add new func

--- test2.py
+++ test2.py
@@ -1,11 +1,1 @@
-print('введите время в секундах, до 86400')
-time=int(input())
-if time < 86401:
-    h=time//3600 
-    min=(time-h*3600)//60
-    sec=(time-h*3600)-(min*60)
-    print(h,' ',min,' ',sec )
-else:
-    print('wrong number')
-    print("(h+min+sec)*0")git as dfg
- ads
+wrfd g
